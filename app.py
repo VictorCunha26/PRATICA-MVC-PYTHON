270a1d3c2f2ee27334f15861c206adc148fbedbb
VictorCunha26 <vicentinivictor130@gmail.com>
2025-04-01 11:08:46 -0300

    Trabalhando

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from model.tarefa import Tarefa
-
+ 
 app = Flask(__name__)
-
-@app.route('/', methods = ['GET', 'POST'])
+ 
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        titulo = request.form['titulo']
-        data_conclusao = request.form['data_conclusao']
-        tarefa = Tarefa(titulo=titulo, data_conclusao=data_conclusao)
+    if request.method == "POST":
+        titulo = request.form["titulo"]
+        data_conclusao = request.form["data_conclusao"]
+        tarefa = Tarefa(titulo = titulo, data_conclusao = data_conclusao)
         tarefa.salvarTarefa()
-        return redirect(url_for('index'))
-
+        return redirect(url_for("index"))
+ 
     tarefas = Tarefa.listarTarefa()
-    return render_template('index.html', tarefas=tarefas,  title = 'Minhas Tarefas')
-
-@app.route('/delete/<int:idTarefa>')
+    return render_template("index.html", tarefas=tarefas, title="Minhas Tarefas")
+ 
+@app.route("/delete/<int:idTarefa>")
 def delete(idTarefa):
     Tarefa.apagarTarefa(idTarefa)
-    return redirect(url_for('index'))
-
+    return redirect(url_for("index"))
+ 
 @app.route("/edit/<int:idTarefa>", methods=["GET", "POST"])
 def edit(idTarefa):
     if request.method == "POST":
@@ -29,3 +29,6 @@ def edit(idTarefa):
         tarefa = Tarefa(titulo=titulo, data_conclusao=data_conclusao, id=idTarefa)
         tarefa.atualizarTarefa()
         return redirect(url_for("index"))
+    
+    tarefas = Tarefa.listarTarefa()
+    return render_template("index.html", tarefas=tarefas, title="Minhas Tarefas")
--- model/tarefa.py
+++ model/tarefa.py
@@ -1,49 +1,45 @@
 from model.database import Database
-
+ 
 class Tarefa:
-    def __init__(self, titulo, id=None, data_conclusao=None):
-        self.id = id
+    def __init__(self, titulo, data_conclusao = None, id= None):
         self.titulo = titulo
-        self.data_conclusao = data_conclusao 
-        
-
-    def salvarTarefa(self): 
-        """Salva uma nova tarefa no banco de dados."""
+        self.id = id
+        self.data_conclusao = data_conclusao
+ 
+    def salvarTarefa(self):
+        """Salva uma nova tarefa no banco de dados!?"""
         db = Database()
         db.conectar()
-
+ 
         sql = 'INSERT INTO tarefa (titulo, data_conclusao) VALUES (%s, %s)'
         params = (self.titulo, self.data_conclusao)
-
         db.executar(sql, params)
         db.desconectar()
-    @staticmethod
+ 
     def listarTarefa():
-        """Retornar uma lista com todas as tarefas cadastradas."""
+        """Retornar uma lista com todas as tarefas..."""
         db = Database()
         db.conectar()
-
+ 
         sql = 'SELECT id, titulo, data_conclusao FROM tarefa'
         tarefas = db.consultar(sql)
         db.desconectar()
         return tarefas if tarefas else []
+   
     @staticmethod
     def apagarTarefa(idTarefa):
-        """Apagar uma tarefa cadastrada no banco de dados."""
+        """Apaga uma tarefa cadastrada no banco de dados?"""
         db = Database()
         db.conectar()
-
         sql = 'DELETE FROM tarefa WHERE id = %s'
-        params = (idTarefa,) # Precisa passar como tupla (a, b, c, ...)
+        params = (idTarefa,) # Precisa ser uma tupla!
         db.executar(sql, params)
         db.desconectar()
-    
+ 
     def atualizarTarefa(self):
         db = Database()
         db.conectar()
-        sql = 'UPDATE tarefa SET titulo = %s,  data_conclusao = %s, WHERE id = %s'
-        params = (self.id, self.titulo, self.data_conclusao, )
+        sql = "UPDATE tarefa SET titulo = %s, data_conclusao = %s WHERE id = %s"
+        params = (self.titulo, self.data_conclusao, self.id)
         db.executar(sql, params)
         db.desconectar()
-
-    
